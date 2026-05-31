bc035d2545ce0058a6e32a88ed5cee7ef1482c9f
TigrRooT <tigrroot@gmail.com>
2026-05-31 17:31:49 +0300

    fix allure

--- tests/tests_site.py
+++ tests/tests_site.py
@@ -3,7 +3,6 @@ import allure
 
 BASE_URL = os.getenv("SITE_URL", "http://google")
 
-
 @allure.feature("Главная страница")
 @allure.story("Открытие страницы")
 @allure.title("Проверка, что страница успешно открывается1")
